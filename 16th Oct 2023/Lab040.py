561f53141f27f889e828c7abca69a82b75712791
milind <milindtrivedi94@gmail.com>
2023-10-31 15:02:24 +0530

    commit from linux

---
+++ 16th Oct 2023/Lab040.py
@@ -0,0 +1,9 @@
+# Calculate the area of a circle
+import math     #import math library
+
+radius = float(input("Enter the radius\n"))
+#area = 3.14* (radius **2)
+area = math.pi * (radius **2)
+
+print(area)
+
