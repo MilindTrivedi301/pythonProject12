df94b133452569d86ae457e718fb90768be5a867
milind <milindtrivedi94@gmail.com>
2023-11-07 16:25:42 +0530

    commit from linux

--- 3rd Nov 2023/Lab111.py
+++ 3rd Nov 2023/Lab111.py
@@ -6,3 +6,14 @@ class Myclass:
 
 pramod_obj_ref = Myclass()
 pramod_obj_ref.print_my_name_with_last_name("Milind", "Trivedi", 24)
+
+
+#-----------------------------------------------------------------
+#name = None
+
+#    def print_my_name(self):
+#        print("Your name is ->" + self.name)
+
+#    name_obj_ref = Myclass()
+#    name_obj_ref.name = "Milind"
+#    name_obj_ref.print_my_name()
